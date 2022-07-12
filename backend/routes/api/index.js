9fcfdffd4a191429dafd5b7664ee3640061555ae
scottwong <wong.sctt@gmail.com>
2022-07-12 16:57:58 -0700

    finish phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -21,5 +21,28 @@ router.get('/set-token-cookie', async (_req, res) => {
 });
 
 
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.use(restoreUser);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
 
 module.exports = router;
